28f5761ca28c233456116c67b30713f4371ffd76
derui <derutakayu@gmail.com>
2017-12-23 00:25:18 +0900

    Fix configuration for counsel

--- conf/init.d/18-ivy.el
+++ conf/init.d/18-ivy.el
@@ -12,8 +12,8 @@
 (use-package counsel
   :config
   (progn
-    (global-set-key (kbd "M-x" #'counsel-M-x))
-    (global-set-key (kbd "C-x C-f" #'counsel-find-file))
+    (global-set-key (kbd "M-x") #'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") #'counsel-find-file)
     (defvar counsel-find-file-ignore-regexp (regexp-opt '("./" "../")))))
 
 (use-package swiper
