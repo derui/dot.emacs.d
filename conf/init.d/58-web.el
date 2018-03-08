a2ab982e4c9b5e731466997603865a0a5d2ac8f7
derui <derutakayu@gmail.com>
2018-03-09 08:19:20 +0900

    fix: use company-css

--- conf/init.d/58-web.el
+++ conf/init.d/58-web.el
@@ -2,17 +2,18 @@
   (require 'use-package))
 
 (use-package css-mode)
+(use-package company-css
+  :commands (company-css))
+
 (use-package scss-mode
   :mode ("\\.scss" . scss-mode)
   :config
+  (setq scss-compile-at-save nil)
   (defun my:scss-mode-hook-0 ()
     (setq-local css-indent-offset 2)
     (setq-local company-backends '(company-semantic
                                    company-files
-                                   company-css-property-alist
-                                   company-css-pseudo-classes
-                                   company-css-value-classes))
-    (setq scss-compile-at-save nil))
+                                   company-css)))
   (add-hook 'scss-mode-hook 'my:scss-mode-hook-0))
 
 ;; yaml
