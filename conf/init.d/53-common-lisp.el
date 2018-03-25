26a3faac58aa48a71b2dc11b055fbfeae3041af1
derui <derutakayu@gmail.com>
2018-03-25 10:47:22 +0900

    fix: add common-lisp configuration

---
+++ conf/init.d/53-common-lisp.el
@@ -0,0 +1,31 @@
+(eval-when-compile
+  (require 'use-package))
+
+;;; (@* "common lisp関係の設定")
+(use-package company
+  :commands (company-mode-on))
+
+(use-package evil-cleverparens
+  :commands (evil-cleverparens-mode))
+
+(use-package aggressive-indent
+  :commands (aggressive-indent-mode))
+
+(use-package lisp-mode
+  :ensure nil
+  :config
+  (defun my:lisp-hooks ()
+    (setq-local company-idle-delay 0.2)
+    (setq-local show-paren-style 'expression)
+
+    (aggressive-indent-mode)
+    (evil-cleverparens-mode)
+    (company-mode-on)
+    (set-newline-and-indent))
+
+  (add-hook 'lisp-mode-hook 'my:lisp-hooks))
+
+(let ((helper (expand-file-name "helper.el" my:roswell-path)))
+  (when (file-exists-p helper)
+    (defvar roswell-slime-contribs '(slime slime-company))
+    (load helper)))
