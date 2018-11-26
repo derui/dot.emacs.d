039c7b66b58c05121b33583f629bfd8a47bbf6da
derui <derutakayu@gmail.com>
2018-11-26 22:18:32 +0900

    style: fix indentation

--- conf/init.d/11-company.el
+++ conf/init.d/11-company.el
@@ -5,49 +5,48 @@
   :commands (global-company-mode)
   :diminish (company-mode . "")
   :config
-  (progn
-    (global-set-key (kbd "C-M-i") 'company-complete-common-or-cycle)
+  (global-set-key (kbd "C-M-i") 'company-complete-common-or-cycle)
 ;;; C-n, C-pで補完候補を選べるように
-    (define-key company-active-map (kbd "M-n") nil)
-    (define-key company-active-map (kbd "M-p") nil)
-    (define-key company-active-map (kbd "C-n") 'company-select-next)
-    (define-key company-active-map (kbd "C-p") 'company-select-previous)
-    (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
-    (define-key company-search-map (kbd "C-n") 'company-select-next)
-    (define-key company-search-map (kbd "C-p") 'company-select-previous)
-    ;; ;のキーで現在選択している候補で保管する
-    (define-key company-active-map (kbd ";") 'company-complete-selection)
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+  (define-key company-search-map (kbd "C-n") 'company-select-next)
+  (define-key company-search-map (kbd "C-p") 'company-select-previous)
+  ;; ;のキーで現在選択している候補で保管する
+  (define-key company-active-map (kbd ";") 'company-complete-selection)
 ;;; C-hがデフォルトでドキュメント表示にmapされているので、文字を消せるようにmapを外す
-    (define-key company-active-map (kbd "C-h") nil)
+  (define-key company-active-map (kbd "C-h") nil)
 ;;; 1つしか候補がなかったらtabで補完、複数候補があればtabで次の候補へ行くように
-    (define-key company-active-map (kbd "C-M-i") 'company-complete-common-or-cycle)
+  (define-key company-active-map (kbd "C-M-i") 'company-complete-common-or-cycle)
 ;;; ドキュメント表示
-    (define-key company-active-map (kbd "M-d") 'company-show-doc-buffer)
+  (define-key company-active-map (kbd "M-d") 'company-show-doc-buffer)
 
-    (setq company-minimum-prefix-length 1) ;; 1文字入力で補完されるように
- ;;; 候補の一番上でselect-previousしたら一番下に、一番下でselect-nextしたら一番上に行くように
-    (setq company-selection-wrap-around t)
-    (setq company-tooltip-align-annotations t)
+  (setq company-minimum-prefix-length 1) ;; 1文字入力で補完されるように
+;;; 候補の一番上でselect-previousしたら一番下に、一番下でselect-nextしたら一番上に行くように
+  (setq company-selection-wrap-around t)
+  (setq company-tooltip-align-annotations t)
 
 ;;; 色の設定。出来るだけ奇抜にならないように
-    (set-face-attribute 'company-tooltip nil
-                        :foreground "black"
-                        :background "lightgray")
-    (set-face-attribute 'company-preview-common nil
-                        :foreground "dark gray"
-                        :background "black"
-                        :underline t)
-    (set-face-attribute 'company-tooltip-selection nil
-                        :background "steelblue"
-                        :foreground "white")
-    (set-face-attribute 'company-tooltip-common nil
-                        :foreground "black"
-                        :underline t)
-    (set-face-attribute 'company-tooltip-common-selection nil
-                        :foreground "white"
-                        :background "steelblue"
-                        :underline t)
-    (set-face-attribute 'company-tooltip-annotation nil
-                        :foreground "red")
+  (set-face-attribute 'company-tooltip nil
+                      :foreground "black"
+                      :background "lightgray")
+  (set-face-attribute 'company-preview-common nil
+                      :foreground "dark gray"
+                      :background "black"
+                      :underline t)
+  (set-face-attribute 'company-tooltip-selection nil
+                      :background "steelblue"
+                      :foreground "white")
+  (set-face-attribute 'company-tooltip-common nil
+                      :foreground "black"
+                      :underline t)
+  (set-face-attribute 'company-tooltip-common-selection nil
+                      :foreground "white"
+                      :background "steelblue"
+                      :underline t)
+  (set-face-attribute 'company-tooltip-annotation nil
+                      :foreground "red")
 
-    (global-company-mode 1)))
+  (global-company-mode 1))
