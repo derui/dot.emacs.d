48efa6467393c140e4437cfb9ac45212ceafa562
derui <derutakayu@gmail.com>
2018-11-26 22:18:16 +0900

    feat: add tide

--- conf/init.d/58-typescript.el
+++ conf/init.d/58-typescript.el
@@ -1,25 +1,35 @@
 (eval-when-compile
   (require 'use-package))
 
+(use-package tide
+  :ensure t)
+
 (use-package typescript-mode
-  :mode ("\\.ts$" . typescript-mode)
+  :ensure t
+  :mode (("\\.ts$" . typescript-mode)
+         ("\\.tsx$" . web-mode))
+  :bind (:map typescript-mode-map
+              ("<C-Tab>" . company-tide))
   :config
   (use-package company :commands (company-mode-on))
-  (use-package flycheck :commands (flycheck-mode flycheck-add-next-checker))
-  (use-package tide
-    :commands (tide-setup)
-    :defines (company-tide)
-    :bind (:map typescript-mode-map
-                ("<C-Tab>" . company-tide))
-    :config
-    (flycheck-add-next-checker 'typescript-tide 'typescript-tslint 'append))
+  (use-package flycheck :commands (flycheck-mode flycheck-add-mode))
+
+  (defun my:web-mode-hook-enable-jsx ()
+    (when (string-equal "tsx" (file-name-extension buffer-file-name))
+      (my:typescript-mode-hook)))
 
   (defun my:typescript-mode-hook ()
     (setq typescript-indent-level 2)
 
     (tide-setup)
-    (flycheck-mode t)
+    (flycheck-mode +1)
+    (make-local-variable 'flycheck-check-syntax-automatically)
+    (setq-local flycheck-check-syntax-automatically '(save mode-enabled))
+    (tide-hl-identifier-mode +1)
+    (eldoc-mode +1)
     ;; (eldoc-mode t)
-    (company-mode-on))
+    (company-mode +1))
 
-  (add-hook 'typescript-mode-hook 'my:typescript-mode-hook))
+  (flycheck-add-mode 'typescript-tslint 'web-mode)
+  (add-hook 'web-mode-hook #'my:web-mode-hook-enable-jsx)
+  (add-hook 'typescript-mode-hook #'my:typescript-mode-hook))
