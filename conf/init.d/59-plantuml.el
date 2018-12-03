15bc24b06b2f8e0e7fd45a58b0ed7061f35d1421
derui <derutakayu@gmail.com>
2018-12-03 22:04:07 +0900

    fix: add options for plantuml

--- conf/init.d/59-plantuml.el
+++ conf/init.d/59-plantuml.el
@@ -2,6 +2,7 @@
   (require 'use-package))
 
 (use-package plantuml-mode
+  :defines (plantuml-output-type plantuml-options)
   :commands (plantuml-mode)
   :config
   (let ((plantuml-jar-file (locate-user-emacs-file "plantuml.jar")))
@@ -9,4 +10,6 @@
       (call-process "curl" nil nil t "-L" "-o" plantuml-jar-file
                     "http://sourceforge.net/projects/plantuml/files/plantuml.jar/download"))
 
+    (setq plantuml-output-type "png")
+    (setq plantuml-options "-charset UTF-8")
     (setq-default plantuml-jar-path plantuml-jar-file)))
