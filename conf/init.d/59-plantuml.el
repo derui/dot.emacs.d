1529eba6741ab7502c47027921ca7d7857699311
derui <derutakayu@gmail.com>
2018-12-03 22:08:42 +0900

    fix: fix to download plantuml.jar

--- conf/init.d/59-plantuml.el
+++ conf/init.d/59-plantuml.el
@@ -5,10 +5,10 @@
   :defines (plantuml-output-type plantuml-options)
   :commands (plantuml-mode)
   :config
-  (let ((plantuml-jar-file (locate-user-emacs-file "plantuml.jar")))
+  (let ((plantuml-jar-file (expand-file-name (locate-user-emacs-file "plantuml.jar"))))
     (unless (file-exists-p plantuml-jar-file)
       (call-process "curl" nil nil t "-L" "-o" plantuml-jar-file
-                    "http://sourceforge.net/projects/plantuml/files/plantuml.jar/download"))
+                    "https://sourceforge.net/projects/plantuml/files/plantuml.jar/download"))
 
     (setq plantuml-output-type "png")
     (setq plantuml-options "-charset UTF-8")
