6837bbe563819d1de09a0c96b6701cf24aca6213
derui <derutakayu@gmail.com>
2019-04-28 10:51:24 +0900

    feat: use rich information in othew-window version switch-buffer

--- conf/package-config.el
+++ conf/package-config.el
@@ -299,17 +299,21 @@ Use fast alternative if it exists, fallback grep if no alternatives in system.
             (all-the-icons-icon-for-mode 'fundamental-mode)
           icon))))
 
-  (plist-put ivy-rich-display-transformers-list
-             'ivy-switch-buffer
-             '(:columns
-               ((ivy-rich-switch-buffer-icon :width 2)
-                (ivy-rich-candidate (:width 30))
-                (ivy-rich-switch-buffer-size (:width 7))
-                (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
-                (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
-                (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
-               :predicate
-               (lambda (cand) (get-buffer cand)))))
+  (let ((rich-transformer-config '(:columns
+                                   ((ivy-rich-switch-buffer-icon :width 2)
+                                    (ivy-rich-candidate (:width 30))
+                                    (ivy-rich-switch-buffer-size (:width 7))
+                                    (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
+                                    (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
+                                    (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
+                                   :predicate
+                                   (lambda (cand) (get-buffer cand)))))
+    (plist-put ivy-rich-display-transformers-list
+               'ivy-switch-buffer
+               rich-transformer-config)
+    (plist-put ivy-rich-display-transformers-list
+               'ivy-switch-buffer-other-window
+               rich-transformer-config)))
 
 ;; mozc
 (when (boundp 'my:mozc-helper-locate)
