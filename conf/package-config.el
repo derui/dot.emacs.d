d1b2a7328d144e56cbc88d46063a87ad180fad8f
derui <derutakayu@gmail.com>
2019-07-06 19:55:03 +0900

    fix: remove popup-dependent prompt

--- conf/package-config.el
+++ conf/package-config.el
@@ -824,54 +824,7 @@ Use fast alternative if it exists, fallback grep if no alternatives in system.
               ("<tab>" . nil)
               ("<C-tab>" . yas-expand))
   :commands (yas-expand yas-global-mode)
-  :hook ((after-init . yas-global-mode))
-  :config
-  (defun yas-popup-isearch-prompt (prompt choices &optional display-fn)
-    (let ((group-max-len 0)
-          (key-max-len 0)
-          (fmt "")
-          (popup-items))
-
-      (mapc #'(lambda (choice)
-                (when (yas--template-p choice)
-                  (setq group-max-len (max group-max-len
-                                           (+ (length (yas--template-group choice) )
-                                              (apply '+ (mapcar 'length (yas--template-group choice))))))
-                  (setq key-max-len (max key-max-len (length (yas--template-key choice))))))
-            choices)
-
-      (setq fmt (format "%s%%%d.%ds%s%%-%d.%ds  %%s"
-                        (if (> group-max-len 0 ) "" " ")
-                        group-max-len group-max-len
-                        (if (> group-max-len 0 ) " > " "")
-                        key-max-len key-max-len))
-
-      (setq popup-items
-            (mapcar
-             #'(lambda (choice)
-                 (popup-make-item
-                  (if (yas--template-p choice)
-                      (format fmt
-                              (if (yas--template-group choice)
-                                  (s-join "/" (yas--template-group choice))
-                                "")
-                              (if (yas--template-key choice)
-                                  (yas--template-key choice)
-                                "")
-                              (if (yas--template-name choice)
-                                  (yas--template-name choice)
-                                ""))
-                    (format " %s" choice))
-                  :value choice))
-             choices))
-
-      (popup-menu*
-       popup-items
-       :prompt prompt
-       :max-width 80
-       :isearch t)))
-
-  (setq yas-prompt-functions '(yas-popup-isearch-prompt)))
+  :hook ((after-init . yas-global-mode)))
 
 ;; googleのコーティング規約に依存するための設定
 (use-package google-c-style
