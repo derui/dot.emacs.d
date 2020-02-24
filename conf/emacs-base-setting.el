7610875f72305bd013036dc58034246df13114ab
derui <derutakayu@gmail.com>
2020-02-24 11:20:36 +0900

    fix: avoid to suspend frame

--- conf/emacs-base-setting.el
+++ conf/emacs-base-setting.el
@@ -172,39 +172,43 @@
   (leaf *global
     :config
     ;; Emacs標準機能関係
-    (global-set-key (kbd "C-h") 'backward-delete-char)
-    (global-set-key (kbd "M-?") 'help-for-help)
-    (global-set-key (kbd "M-d") 'my:kill-word-at-point)
-    (global-set-key (kbd "C-m") 'newline-and-indent)
-    (global-set-key (kbd "C-x /") 'dabbrev-expand)
-    (global-set-key (kbd "C-x ,") 'delete-region)
-    (global-set-key (kbd "M-;") 'comment-dwim)
-    (global-set-key (kbd "C-x C-b") 'ibuffer)
-    (global-set-key (kbd "C-_") 'redo)
+    ;; suspend-frameは利用しない
+    (global-set-key (kbd "C-z") nil)
+    (global-set-key (kbd "C-h") #'backward-delete-char)
+    (global-set-key (kbd "M-?") #'help-for-help)
+    (global-set-key (kbd "M-d") #'my:kill-word-at-point)
+    (global-set-key (kbd "C-m") #'newline-and-indent)
+    (global-set-key (kbd "C-x /") #'dabbrev-expand)
+    (global-set-key (kbd "C-x ,") #'delete-region)
+    (global-set-key (kbd "M-;") #'comment-dwim)
+    (global-set-key (kbd "C-x C-b") #'ibuffer)
+    (global-set-key (kbd "C-_") #'redo)
 
     ;; flymake関連
-    (global-set-key (kbd "C-c d") 'credmp/flymake-display-err-minibuf)
+    (global-set-key (kbd "C-c d") #'credmp/flymake-display-err-minibuf)
 
     ;; マウスのホイールスクロールスピードを調節
     ;; (連続して回しているととんでもない早さになってしまう。特にLogicoolのマウス)
-    (global-set-key [wheel-up] '(lambda () "" (interactive) (scroll-down 1)))
-    (global-set-key [wheel-down] '(lambda () "" (interactive) (scroll-up 1)))
-    (global-set-key [double-wheel-up] '(lambda () "" (interactive) (scroll-down 1)))
-    (global-set-key [double-wheel-down] '(lambda () "" (interactive) (scroll-up 1)))
-    (global-set-key [triple-wheel-up] '(lambda () "" (interactive) (scroll-down 2)))
-    (global-set-key [triple-wheel-down] '(lambda () "" (interactive) (scroll-up 2)))
+    (global-set-key [wheel-up] #'(lambda () "" (interactive) (scroll-down 1)))
+    (global-set-key [wheel-down] #'(lambda () "" (interactive) (scroll-up 1)))
+    (global-set-key [double-wheel-up] #'(lambda () "" (interactive) (scroll-down 1)))
+    (global-set-key [double-wheel-down] #'(lambda () "" (interactive) (scroll-up 1)))
+    (global-set-key [triple-wheel-up] #'(lambda () "" (interactive) (scroll-down 2)))
+    (global-set-key [triple-wheel-down] #'(lambda () "" (interactive) (scroll-up 2)))
 
     ;; org-mode関係
-    (global-set-key (kbd "C-c l") 'org-store-link)
-    (global-set-key (kbd "C-c a") 'org-agenda)
-    (global-set-key (kbd "C-c c")  'org-capture))
+    (global-set-key (kbd "C-c l") #'org-store-link)
+    (global-set-key (kbd "C-c a") #'org-agenda)
+    (global-set-key (kbd "C-c c")  #'org-capture)
+    ;; ivy
+    (global-set-key (kbd "C-;") #'ivy-switch-buffer))
 
   (leaf *map-local
     :config
-    (define-key read-expression-map (kbd "TAB") 'lisp-complete-symbol)
+    (define-key read-expression-map (kbd "TAB") #'lisp-complete-symbol)
 
     ;; isearch
-    (define-key isearch-mode-map (kbd "C-h") 'isearch-delete-char)))
+    (define-key isearch-mode-map (kbd "C-h") #'isearch-delete-char)))
 
 (leaf dired
   :require t
