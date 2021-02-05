dc78fae823dcc433d9fce8b865cff4266d8b8de6
derui <derutakayu@gmail.com>
2021-02-06 08:45:57 +0900

    fix: add some configurations for ddskk

--- init-ddskk.el
+++ init-ddskk.el
@@ -1,1 +1,7 @@
 (setq skk-sticky-key ";")
+;; 送り仮名が厳密に正しいものを優先して表示するようにする
+(setq skk-henkan-strict-okuri-precedence t)
+;; 変換モードでEnterを押しても改行しないように
+(setq skk-egg-like-newline t)
+;; 個人辞書の文字コード
+(setq skk-jisyo-code 'utf-8-unix)
