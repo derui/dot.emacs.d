1af1a9bd4bfbafdc73892ad8ef69e5e51c307620
derui <derutakayu@gmail.com>
2024-06-24 19:50:14 +0900

    feat: add some profiler to track loading elisp

--- init.el
+++ init.el
@@ -1,2 +1,2 @@
 ;; -*- coding: utf-8 -*-
-(progn (require 'org) (org-babel-tangle-file (expand-file-name "init.org" user-emacs-directory) (expand-file-name "init.el" user-emacs-directory)) (load (expand-file-name "init.el" user-emacs-directory)) (message "Once kill emacs for apply new init.el written from init.org") (setq kill-emacs-hook nil) (kill-emacs nil t))
+(progn (require 'org) (org-babel-tangle-file (expand-file-name "init.org" user-emacs-directory) (expand-file-name "init.el" user-emacs-directory)) (batch-byte-compile-file (expand-file-name "init.el" user-emacs-directory)) (message "Once kill emacs for apply new init.el written from init.org") (setq kill-emacs-hook nil) (kill-emacs nil t))
