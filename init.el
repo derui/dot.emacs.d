9c55d1492cf4d1b32adf2eb6b902b0ee476141e3
derui <derutakayu@gmail.com>
2026-08-01 08:09:38 +0900

    refactor: update qwen model version to 35b

--- init.el
+++ init.el
@@ -4262,7 +4262,7 @@ When it is nil or not passed, run `select-window' with returned window by `comma
             :stream t
             :protocol "http"
             :host "localhost:9292"
-            :models '(gemma-4-nt:12b qwen3.6:27b))))
+            :models '(gemma-4-nt:12b qwen3.6:35b))))
 
 (with-low-priority-startup (load-package gptel))
 
